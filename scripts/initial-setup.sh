f4aa0c38ef49d9cbcaae0f0236229e4d0883e15c
grauwen <51956682+grauwen@users.noreply.github.com>
2025-10-13 13:51:28 +0200

    Create initial-setup.sh

---
+++ scripts/initial-setup.sh
@@ -0,0 +1,12 @@
+# Clone the repository
+git clone https://github.com/grauwen/utl-x.git
+cd utl-x
+
+# Run setup script (creates entire structure)
+make setup
+
+# Download Gradle wrapper
+make gradle-wrapper
+
+# Build project
+make build
