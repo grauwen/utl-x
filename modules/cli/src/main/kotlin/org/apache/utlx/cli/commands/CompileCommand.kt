e715ca2f93755cef82434ab2d15a51c3040aac62
grauwen <51956682+grauwen@users.noreply.github.com>
2025-10-13 23:41:53 +0200

    Update CompileCommand.kt

--- modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/CompileCommand.kt
+++ modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/CompileCommand.kt
@@ -14,28 +14,4 @@ object CompileCommand {
     }
 }
 
-// modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/VersionCommand.kt
-package org.apache.utlx.cli.commands
 
-object VersionCommand {
-    private const val VERSION = "1.0.0-SNAPSHOT"
-    private const val BUILD_DATE = "2025-01-15"
-    
-    fun execute(args: Array<String>) {
-        val verbose = args.contains("-v") || args.contains("--verbose")
-        
-        println("UTL-X v$VERSION")
-        
-        if (verbose) {
-            println("Build date: $BUILD_DATE")
-            println("Kotlin version: ${KotlinVersion.CURRENT}")
-            println("JVM version: ${System.getProperty("java.version")}")
-            println("OS: ${System.getProperty("os.name")} ${System.getProperty("os.version")}")
-            println("Architecture: ${System.getProperty("os.arch")}")
-            
-            println("\nProject: https://github.com/grauwen/utl-x")
-            println("Documentation: https://utlx-lang.org/docs")
-            println("License: AGPL-3.0 / Commercial")
-        }
-    }
-}
