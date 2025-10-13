df5418fd1e0e10f41568c5b3ae6d7d74fab04e82
grauwen <51956682+grauwen@users.noreply.github.com>
2025-10-13 13:10:55 +0200

    Update MigrateCommand.kt

--- modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/MigrateCommand.kt
+++ modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/MigrateCommand.kt
@@ -1,1 +1,39 @@
+package org.apache.utlx.cli.commands
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.choice
+import com.github.ajalt.clikt.parameters.types.file
+
+/**
+ * Migrate command - Convert XSLT/DataWeave to UTL-X
+ */
+class MigrateCommand : CliktCommand(
+    name = "migrate",
+    help = "Migrate XSLT or DataWeave transformations to UTL-X"
+) {
+    
+    private val source by argument(
+        name = "SOURCE",
+        help = "Source transformation file"
+    ).file(mustExist = true, mustBeReadable = true)
+    
+    private val from by option(
+        "-f", "--from",
+        help = "Source format"
+    ).choice("xslt", "dataweave").default("xslt")
+    
+    private val output by option(
+        "-o", "--output",
+        help = "Output UTL-X file"
+    ).file()
+    
+    override fun run() {
+        echo("Migrating from $from: ${source.absolutePath}")
+        
+        // TODO: Implement migration
+        echo("TODO: Implement migration from $from")
+    }
+}
