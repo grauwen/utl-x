bdd840098d9bc177b12b0b5ef4b0abc52110d3aa
grauwen <51956682+grauwen@users.noreply.github.com>
2025-10-13 17:29:39 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ include("modules:javascript")
 include("modules:native")
 include("modules:cli")
 
-// Format modules
+// Format parsers/serializers
 include("formats:xml")
 include("formats:json")
 include("formats:csv")
@@ -17,9 +17,17 @@ include("formats:plugin")
 // Standard library
 include("stdlib")
 
-// Tools
+// Development tools
 include("tools:vscode-extension")
 include("tools:intellij-plugin")
 include("tools:maven-plugin")
 include("tools:gradle-plugin")
 include("tools:benchmarks")
+
+// Enable Gradle build cache
+buildCache {
+    local {
+        isEnabled = true
+        directory = file("${rootDir}/.gradle/build-cache")
+    }
+}
