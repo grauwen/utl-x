a100b27967d746b8f19ff43c9eefffcfd74b71dd
grauwen <51956682+grauwen@users.noreply.github.com>
2025-10-13 23:41:07 +0200

    Update CompileCommand.kt

--- modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/CompileCommand.kt
+++ modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/CompileCommand.kt
@@ -13,52 +13,6 @@ object CompileCommand {
         println("This will compile UTL-X scripts to optimized bytecode")
     }
 }
-// modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/MigrateCommand.kt
-package org.apache.utlx.cli.commands
-
-import kotlin.system.exitProcess
-
-object MigrateCommand {
-    fun execute(args: Array<String>) {
-        if (args.isEmpty()) {
-            printUsage()
-            exitProcess(1)
-        }
-        
-        val sourceType = args.getOrNull(0)?.let {
-            when (it.lowercase()) {
-                "xslt", "dataweave", "dw" -> it
-                else -> null
-            }
-        }
-        
-        if (sourceType == null) {
-            System.err.println("Error: Invalid source type")
-            printUsage()
-            exitProcess(1)
-        }
-        
-        println("Migration from $sourceType - Coming soon!")
-        println("This will help you migrate XSLT/DataWeave scripts to UTL-X")
-    }
-    
-    private fun printUsage() {
-        println("""
-            |Migrate XSLT or DataWeave scripts to UTL-X
-            |
-            |Usage:
-            |  utlx migrate <type> <source-file> [options]
-            |
-            |Types:
-            |  xslt        Migrate from XSLT
-            |  dataweave   Migrate from DataWeave
-            |
-            |Options:
-            |  -o, --output FILE   Write output to FILE
-            |  -h, --help          Show this help message
-        """.trimMargin())
-    }
-}
 
 // modules/cli/src/main/kotlin/org/apache/utlx/cli/commands/VersionCommand.kt
 package org.apache.utlx.cli.commands
