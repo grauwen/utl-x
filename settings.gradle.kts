888eff69789e54e4bd045dcec7a6eddbfc65cbba
grauwen <51956682+grauwen@users.noreply.github.com>
2025-10-14 00:06:22 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,33 +1,70 @@
 rootProject.name = "utl-x"
 
+// Future modules (commented out until implemented)
+// include(":modules:jvm")
+// include(":modules:javascript")
+// include(":modules:native")
+// include(":stdlib")
+// include(":formats:yaml")
+// include(":formats:plugin")
+// include(":tools:vscode-extension")
+// include(":tools:intellij-plugin")
+// include(":tools:maven-plugin")
+// include(":tools:gradle-plugin")
+// include(":tools:benchmarks")
+
+
+
 // Core modules
 include("modules:core")
-include("modules:jvm")
-include("modules:javascript")
-include("modules:native")
+//include("modules:jvm")
+//include("modules:javascript")
+//include("modules:native")
 include("modules:cli")
+
 
 // Format parsers/serializers
 include("formats:xml")
 include("formats:json")
 include("formats:csv")
-include("formats:yaml")
-include("formats:plugin")
+//include("formats:yaml")
+//include("formats:plugin")
 
 // Standard library
-include("stdlib")
+//include("stdlib")
 
 // Development tools
-include("tools:vscode-extension")
-include("tools:intellij-plugin")
-include("tools:maven-plugin")
-include("tools:gradle-plugin")
-include("tools:benchmarks")
+//include("tools:vscode-extension")
+//include("tools:intellij-plugin")
+//include("tools:maven-plugin")
+//include("tools:gradle-plugin")
+//include("tools:benchmarks")
 
 // Enable Gradle build cache
 buildCache {
     local {
         isEnabled = true
         directory = file("${rootDir}/.gradle/build-cache")
+    }
+}
+
+// Plugin management
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    
+    plugins {
+        kotlin("jvm") version "1.9.22"
+        id("org.graalvm.buildtools.native") version "0.10.2"
+    }
+}
+
+// Dependency resolution management
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
     }
 }
