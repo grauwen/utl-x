7a1bb92a4e45dbd77dd29c6b92c7cc813d85db7d
Marcel Grauwen <marcel.grauwen@digitect.io>
2025-11-05 13:49:10 +0100

    updates for Theia 1.64 support

--- theia-extension/utlx-theia-extension/src/node/backend-module.ts
+++ theia-extension/utlx-theia-extension/src/node/backend-module.ts
@@ -6,7 +6,7 @@
 
 import { ContainerModule } from 'inversify';
 import { ConnectionHandler, RpcConnectionHandler } from '@theia/core';
-import { UTLXService, UTLX_SERVICE_PATH } from '../common/protocol';
+import { UTLXService, UTLX_SERVICE_PATH, UTLX_SERVICE_SYMBOL } from '../common/protocol';
 import { UTLXServiceImpl } from './services/utlx-service-impl';
 import { UTLXDaemonClient } from './daemon/utlx-daemon-client';
 
@@ -15,12 +15,12 @@ export default new ContainerModule(bind => {
     bind(UTLXDaemonClient).toSelf().inSingletonScope();
 
     // Bind service implementation
-    bind(UTLXService).to(UTLXServiceImpl).inSingletonScope();
+    bind(UTLX_SERVICE_SYMBOL).to(UTLXServiceImpl).inSingletonScope();
 
     // Create RPC connection handler for frontend-backend communication
     bind(ConnectionHandler).toDynamicValue(ctx =>
         new RpcConnectionHandler(UTLX_SERVICE_PATH, () => {
-            return ctx.container.get<UTLXService>(UTLXService);
+            return ctx.container.get<UTLXService>(UTLX_SERVICE_SYMBOL);
         })
     ).inSingletonScope();
 
