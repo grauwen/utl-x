561437818d42bbc9d0ead65e9504931eb475bd37
Marcel Grauwen <marcel.grauwen@digitect.io>
2025-12-30 16:31:41 +0100

    changes to playright settings

--- conformance-suite/theia-extension/playwright.config.ts
+++ conformance-suite/theia-extension/playwright.config.ts
@@ -22,6 +22,9 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
 
+  /* Test timeout - disabled for manual testing (user closes browser) */
+  timeout: 0,
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL for Theia */
